6aba04b92938c3bd33413b46fd2e47b145ca3d8c
Christian Kovacs <ckovacsdev@gmail.com>
2026-08-30 14:16:02 -0400

    fix navigation positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Home } from './pages/home'
+import { Navigation } from './components/navigation/navi'
 import './App.css'
 
 function App() {
 	return (
 		<div className='app-container'>
+			<Navigation />
 			<Home />
 		</div>
 	)
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
 import { DecoPattern } from "../components/deco-pattern/deco-pattern"
 import { DecoRule } from "../components/deco-rule/deco-rule";
-import { Navigation } from "../components/navigation/navi";
 import './home.css';
 
 export const Home = () => {
     return (
         <div className='home-container'>
-            <Navigation />
             <div className='home-content'>
                 <div className='home-text'>
                     <h1 className='home-title'> Christian Kovacs </h1>
