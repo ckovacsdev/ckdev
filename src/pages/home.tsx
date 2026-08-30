cd59ea1b270c3bf19fda13c046a883c4654e2f5a
Christian Kovacs <ckovacsdev@gmail.com>
2026-08-30 02:15:53 -0400

    separator component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,16 @@
 import { DecoPattern } from "../components/deco-pattern/deco-pattern"
+import { DecoRule } from "../components/deco-rule/deco-rule";
 import './home.css';
 
 export const Home = () => {
     return (
         <div className='home-container'>
             <div className='home-content'>
-                
+                <h1> Christian Kovacs </h1>
+                <div className='home-separator'>  
+                    <DecoRule />
+                </div>
+                <h2> Test Subtitle </h2>
             </div>
             <div className='deco-pattern'>
                 <DecoPattern />
